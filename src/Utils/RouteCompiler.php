0580ab3594d59114428f8713a0c6385da9c595f2
Valerio Cervo <v.cervo.90@gmail.com>
2019-02-14 12:43:09 +0100

    Fixed typo in code

--- src/Utils/RouteCompiler.php
+++ src/Utils/RouteCompiler.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 class RouteCompiler
 {
 
-    public static function compile(z$path, $params)
+    public static function compile($path, $params)
     {
 
         foreach($params as $key => $value) {
